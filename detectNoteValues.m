47bc3fb986591917577bfc68017eb2aa09f48b7e
Jessica <jesbe726@student.liu.se>
2013-11-26 16:02:25 +0100

    new cut works but has some problems

--- detectNoteValues.m
+++ detectNoteValues.m
@@ -1,12 +1,15 @@
 function [ noteValues ] = detectNoteValues( removedStaff,img_rot,startStaffSystem, staffSpace, boxes, noteHeads )
 
-debug = 2;
+debug = 0;
 %1 eingefärbte noten
 %2 histogramme
 
-
-cutWidth  = 1.15*staffSpace;
-cutHeight = 1.7*staffSpace;
+% width right and left of note center point
+cutWidth  = ceil(staffSpace);
+% height to cut from small image, to remove note head
+cutHeight = ceil(2*staffSpace);
+% numbre of pixels to add to small image for better note detection
+add = ceil(staffSpace/2.0);
 
 numberOfStaffSystems = length(startStaffSystem);
 noteValues = repmat( struct('data',[]),1,numberOfStaffSystems);
@@ -21,7 +24,7 @@ if debug == 1
     hold on;
 end
 
-if debug == 2
+if debug > 0 
     noteStart = 4;
     numNotes = 1;
     staffStart = 1;
@@ -37,7 +40,7 @@ for staff = staffStart:staffEnd
     
     noteEnd=length(noteHeads(staff).data);
     
-    if debug == 2
+    if debug > 0
         noteEnd= noteStart+numNotes-1;
     end
     
@@ -54,7 +57,10 @@ for staff = staffStart:staffEnd
             
             % if red dot is in aabb
             %boxes(k).dim1
-            if (boxes(k).minX <= noteX) &&(boxes(k).maxX >= noteX) && (boxes(k).minY <= noteY) && (boxes(k).maxY >= noteY)
+                minY = min(boxes(k).y(abs(boxes(k).x-noteX) < cutWidth));
+                maxY = max(boxes(k).y(abs(boxes(k).x-noteX) < cutWidth));
+                
+            if (boxes(k).minX <= noteX) &&(boxes(k).maxX >= noteX) && (minY <= noteY) && (maxY >= noteY)
                 
                 % make projection
                 left =  noteX - cutWidth;
@@ -87,37 +93,70 @@ for staff = staffStart:staffEnd
                 
                 pixelCut = round(noteY);
                 
-                upperHalf =  removedStaff(boxes(k).minY:pixelCut-1    ,left:right);
+                
+                %minY = min(boxes(k).x (mit abs(x-noteX) < width))
+
+                
+                upperHalf =  removedStaff(minY:pixelCut-1    ,left:right);
                 upperHalf = flipdim(upperHalf,1);
-                lowerHalf =  removedStaff(pixelCut     :boxes(k).maxY ,left:right);
+                lowerHalf =  removedStaff(pixelCut     :maxY ,left:right);
                 
                 
                 sUpperHalf = size(upperHalf);
                 sLowerHalf = size(lowerHalf);
                 
+                % get final cut information
+                finalCutLeft = left;
+                finalCutRight = right;
+                finalCutUp = 0;
+                finalCutDown = 0;
+                noteHeadInBottom = true;
+                
                 %increase image to correct size
                 if sUpperHalf(1) > sLowerHalf(1)
+                    noteHeadInBottom = true;
                     lowerHalf = [lowerHalf; zeros(sUpperHalf(1)-sLowerHalf(1),sLowerHalf(2))];
+                    finalCutUp = minY;
+                    finalCutDown = pixelCut-cutHeight;
                     
-                elseif sLowerHalf(1) > sUpperHalf(1)
+                    
+                else %if sLowerHalf(1) >= sUpperHalf(1)
+                    noteHeadInBottom = false;
                     upperHalf =[upperHalf; zeros(sLowerHalf(1)-sUpperHalf(1),sUpperHalf(2))];
+                    finalCutUp = pixelCut+cutHeight;
+                    finalCutDown = maxY;
                 end
                 
                 
-                % cut upper part to get only "faehnchen"
+%                     figure('name','cutbox'), imshow(img_rot);
+%                     hold on;
+%                     
+%                     aabb = [];
+%                     aabb = [aabb; finalCutLeft finalCutUp];
+%                     aabb = [aabb; finalCutLeft finalCutDown];
+%                     aabb = [aabb; finalCutRight  finalCutDown];
+%                     aabb = [aabb; finalCutRight finalCutUp];
+%                     aabb = [aabb; finalCutLeft finalCutUp];
+%                     
+%                     
+%                     plot(aabb(:,1),aabb(:,2), 'c', 'LineWidth', 2);
+%                 
+                
+                %join images
                 res = im2bw(upperHalf + lowerHalf);
                 
                 if debug == 2
-                    figure('name','res'),imshow(res);
+                    %figure('name','res'),imshow(res);
                 end
                 
+                % cut upper part to get only "faehnchen"
                 sizeRes = size(res,1);
                 resultWidth = sizeRes-cutHeight;
                 startCut    = sizeRes-resultWidth;
                 res = res(startCut:sizeRes,:);
                 
                 if debug == 2
-                    figure('name','res'),imshow(res);
+                    %figure('name','res'),imshow(res);
                 end
                 
                 
@@ -129,25 +168,65 @@ for staff = staffStart:staffEnd
                 
                 l2 = length(summeHorizFiltered)/2;
                 l  = length(summeHorizFiltered);
-                leftHorizHalf = mean(summeHorizFiltered(1:l2));
-                rightHorizHalf = mean(summeHorizFiltered(l2:l));
+                leftHorizHalf = max(summeHorizFiltered(1:l2));
+                rightHorizHalf = max(summeHorizFiltered(l2:l));
                 
                 if debug == 2
-                    figure('name','plot of hori projection'),bar(summeHorizFiltered);
+                    %figure('name','plot of hori projection'),bar(summeHorizFiltered);
                 end
                 
                 
+                imgWidth = ceil((finalCutRight - finalCutLeft)/2);
                 
                 if leftHorizHalf > rightHorizHalf
                     res = res(:,1:l2);
+                    finalCutRight = finalCutRight - imgWidth;
+                    finalCutLeft = finalCutLeft - add;
+                    
                 else %leftHorizHalf > rightHorizHalf
                     res = res(:,l2:l);
+                    finalCutLeft = finalCutLeft + imgWidth;
+                    finalCutRight = finalCutRight + add;
+                end
+                
+                if finalCutLeft < 1
+                    finalCutLeft = 1;
+                end
+                
+                if finalCutRight> s(2)
+                    finalCutRight = s(2);
                 end
                 
                 
                 if debug == 2
-                    figure('name','resTotallyCut'),imshow(res);
+                    %figure('name','resTotallyCut'),imshow(res);
                 end
+                
+                    
+                % replace res with increased cut of original image
+                res = removedStaff(finalCutUp:finalCutDown,finalCutLeft:finalCutRight);
+
+                
+                if debug == 2
+                    finalCutUp
+                    finalCutDown
+                    finalCutLeft
+                    finalCutRight
+                    %figure('name','resTotallyCutNeu'),imshow(res);
+                end       
+                    %figure('name','cutbox'), imshow(img_rot);
+                    %hold on;
+                    
+%                     aabb = [];
+%                     aabb = [aabb; finalCutLeft finalCutUp];
+%                     aabb = [aabb; finalCutLeft finalCutDown];
+%                     aabb = [aabb; finalCutRight  finalCutDown];
+%                     aabb = [aabb; finalCutRight finalCutUp];
+%                     aabb = [aabb; finalCutLeft finalCutUp];
+%                     
+%                     
+%                     plot(aabb(:,1),aabb(:,2), 'm', 'LineWidth', 2);
+%                 
                 
                 
                 %vertical projection
